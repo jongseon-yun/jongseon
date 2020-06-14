a2c9ec13c9d74c86f08a54a25bcd4dc438f2dfc0
jongseonyun <hanmail12312@gmail.com>
2020-06-14 14:27:01 +0900

    done

---
+++ 4csharp/csharpprac/Square3.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace csharpprac
+{
+    class Square3
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
