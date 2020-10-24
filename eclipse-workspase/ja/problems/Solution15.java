3adef3dd6cd7b3c37835c64bc11909b37f2dceb2
윤종선 <@DESKTOP-8706QH1>
2020-10-24 19:05:29 +0900

    20201024

---
+++ eclipse-workspase/ja/problems/Solution15.java
@@ -0,0 +1,17 @@
+package problems;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Solution15 {
+	public static void main(String[] args) {
+		
+	}
+
+	public static String[] solution(String[] strings, int n) {
+        String[] answer = {};
+        char compare
+        return answer;
+    }
+}
