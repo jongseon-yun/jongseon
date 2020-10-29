2073e2b156900c3afb4e64ebebac4bcafef2ced5
윤종선 <@DESKTOP-8706QH1>
2020-10-29 13:59:02 +0900

    20201028

---
+++ eclipse-workspase/ja/problems2/Solution.java
@@ -0,0 +1,30 @@
+package problems2;
+
+import java.util.HashMap;
+import java.util.Iterator;
+
+class Solution {
+
+	public static void main(String[] args) {
+		System.out.println(solution(15));
+	}
+
+	/* 모범답안 */
+	public static int solution(int n) {
+		int answer = 0;
+		
+		for (int i = 1; i <= n; i++) {
+			int x = 0;
+			for (int j = i; x <= n; j++) {
+				x += j;
+				if (x == n) {
+					answer += 1;
+				}
+			}
+		}
+		
+		return answer;
+
+	}
+
+}
---
+++ eclipse-workspase/ja/problems2/Solution3.java
@@ -0,0 +1,32 @@
+package problems2;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+class Solution3 {
+
+	public static void main(String[] args) {
+		String arr[] = { "Enter uid1234 Muzi", "Enter uid4567 Prodo", "Leave uid1234", "Enter uid1234 Prodo",
+				"Change uid4567 Ryan" };
+		System.out.println(solution(arr));
+	}
+
+	public static String[] solution(String[] record) {
+		String[] answer = {};
+		Map<String, String> keymap = new HashMap<String, String>();
+
+		for (int i = 0; i < record.length; i++) {
+			String[] temp = record[i].split(" ");
+			for (int j = 0; j < temp.length; j++) {
+				System.out.println(temp[i]);
+			}
+			if (temp[i].equals("Enter") || temp[i].equals("Leave")) {
+
+			}
+		}
+
+		return answer;
+	}
+
+}
