a2b6ce7da6fc7fd6e14862312ae497e63e566486
beaver <hanmail12312@gmail.com>
2020-11-03 23:07:29 +0900

    20201104

---
+++ eclipse-workspase/ja/problems/october2020/level2/wednesday1104/Solution2.java
@@ -0,0 +1,11 @@
+package problems.october2020.level2.wednesday1104;
+
+public class Solution2 {
+	public static void main(String[] args) {
+
+	}
+
+	private static void solution() {
+
+	}
+}
---
+++ eclipse-workspase/ja/problems/october2020/level2/wednesday1104/ans/Solution.java
@@ -0,0 +1,11 @@
+package problems.october2020.level2.wednesday1104.ans;
+
+public class Solution {
+	public static void main(String[] args) {
+
+	}
+
+	private static void solution() {
+
+	}
+}
