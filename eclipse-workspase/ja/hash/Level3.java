310cf008576fe577f02e11a67df25ae1f36532db
beaver <hanmail12312@gmail.com>
2020-11-03 11:42:48 +0900

    20201103-1

---
+++ eclipse-workspase/ja/hash/Level3.java
@@ -0,0 +1,17 @@
+package hash;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class Level3 {
+	public static void main(String[] args) {
+
+	}
+
+	public int solution(String[][] clothes) {
+        int answer = 0;
+        
+        return answer;
+    }
+}
