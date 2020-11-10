0c0f2a39ba6421630875d8c9c9f0ec2de65b8e3a
beaver <hanmail12312@gmail.com>
2020-11-10 15:58:25 +0900

    20201110-1

--- eclipse-workspase/ja/src/problems/nomember2020/level2/tuesday1110/ans/Solution2.java
+++ eclipse-workspase/ja/src/problems/nomember2020/level2/tuesday1110/ans/Solution2.java
@@ -2,27 +2,34 @@ package problems.nomember2020.level2.tuesday1110.ans;
 
 public class Solution2 {
 
-	public int solution(int[] numbers, int target) {
-		return DFS(numbers, target, 0, 0);
+	private static int answer = 0;
+
+	public static int solution(int[] numbers, int target) {
+		dfs(numbers, target, 0);
+		return answer;
 	}
 
-	public int DFS(int[] numbers, int target, int index, int num) {
-		if (index == numbers.length) {
-			return num == target ? 1 : 0;
+	private static void dfs(int[] numbers, int target, int i) {
+		// TODO Auto-generated method stub
+		if (i == numbers.length) {
+			int sum = 0;
+			for (int num : numbers) {
+				sum += num;
+			}
+			if (sum == target) {
+				answer++;
+			}
 		} else {
-			return DFS(numbers, target, index + 1, num + numbers[index])
-					+ DFS(numbers, target, index + 1, num - numbers[index]);
+			numbers[i] *= 1;
+			dfs(numbers, target, i + 1);
+			numbers[i] *= -1;
+			dfs(numbers, target, i + 1);
 		}
 	}
 
 	public static void main(String[] args) {
-		int[] arr = new int[20];
-		for (int i = 0; i < arr.length; i++) {
-			arr[i] = 1;
-		}
+		int[] numbers = { 1, 1, 1, 1, 1 };
 		int target = 3;
-
-		System.out.println(new Solution2().solution(arr, target));
+		System.out.println(solution(numbers, target));
 	}
-
 }
